d63c723cebbb721ed3c28f1a468ae91984533e6f
Ogilo <alphaogilo@gmail.com>
2020-12-17 17:46:53 +0200

    chore: setup basic express server

---
+++ server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.json({
+        hi: "Welcome to the NodeJS 2FA App",
+    });
+});
+
+app.listen(
+    PORT,
+    console.log(`Server running in ${process.env.NODE_ENV} mode 
+on port ${PORT}`)
+);
